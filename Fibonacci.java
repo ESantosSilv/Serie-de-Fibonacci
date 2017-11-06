96d62a1309c83d023b69daa328a56d841813b110
nicoe <nicoe@LAPTOP-VF9ONJKL>
2017-11-06 18:36:50 -0500

    Fibonacci corregido

---
+++ Fibonacci.java
@@ -0,0 +1,20 @@
+public class main {
+		  public static void main (String args []){
+		  //Variables para guardar el dato nuevo y anterior.
+			  int x=1;
+			  int anterior=0;
+			  int temp;
+			  
+			  while(true)
+			  {
+				  System.out.println(x);
+				  temp =x;
+				  x=x+anterior;
+				  anterior=temp;
+				  if(x>100)
+				  {break;}
+			  }	
+				  
+
+		   }
+	}
